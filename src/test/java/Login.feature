18b27d58cb235bd4c99a5b83f62625085a802315
USER <anoopvijay0809@gmail.com>
2020-07-02 12:36:45 +0530

    first commit

---
+++ src/test/java/Login.feature
@@ -0,0 +1,9 @@
+Feature: Login to the web application
+
+
+Scenario: Home page default login
+
+
+Given User is on landing page
+When User login to the application
+Then User is in landing page
---
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -0,0 +1,11 @@
+package stepDefinitions;
+
+public class stepDefinition {
+	
+	@Given("^User is on landing page$")
+	public void user_is_on_landing_page()
+	{
+		System.out.println("The user is in login screen");
+	}
+
+}
